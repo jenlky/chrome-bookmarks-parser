38487ead21aaf4747971a9ccba9a7f2261bb867b
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-11-06 11:48:32 +0800

    Return promise for readFile and add example in test

--- __test__/parser.test.js
+++ __test__/parser.test.js
@@ -1,7 +1,15 @@
 const { readFile } = require("../lib/parser");
+const path = require("path");
 
 describe("readFile", () => {
   it("should throw an error if file path is not provided", () => {
     expect(() => readFile()).toThrow();
+  });
+
+  it("should read file", async () => {
+    const filePath = path.resolve(__dirname, "./files/chrome_bookmarks.html");
+    const data = await readFile(filePath);
+    expect(data).toHaveProperty("parser");
+    expect(data).toHaveProperty("bookmarks");
   });
 });
